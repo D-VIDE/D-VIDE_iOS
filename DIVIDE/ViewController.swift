4f6ce062c2b5bcea1260a103ee2b2ce7df73bb07
enchantee00 <jammy9087@gmail.com>
2022-06-28 17:29:19 +0900

    프로젝트 생성

---
+++ DIVIDE/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DIVIDE
+//
+//  Created by 정지윤 on 2022/06/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
