8101d71964f989f4ee11bdb6ca30ced2c2120796
Henry <wcbe9745@naver.com>
2022-08-09 16:33:14 +0900

    general setting

--- DIVIDE/SceneDelegate.swift
+++ DIVIDE/SceneDelegate.swift
@@ -11,11 +11,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let initialViewController = ChatListViewController()
+        let initialViewController = TabBarController()
         window?.rootViewController = initialViewController
         window?.makeKeyAndVisible()
 
