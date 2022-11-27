230a151e0f093e3d606908fdfe05cbed15b94aa2
Henry <wcbe9745@naver.com>
2022-11-28 00:14:05 +0900

    Refactoring completed.

--- DIVIDE/Page/Profile/ProfileViewController.swift
+++ DIVIDE/Page/Profile/ProfileViewController.swift
@@ -10,45 +10,47 @@ import SnapKit
 
 class ProfileViewController: UIViewController {
     
-    private let mainProfile = UIView().then {
-        $0.borderColor = .mainYellow
-        $0.borderWidth = 3
-        $0.backgroundColor = .white
-        $0.cornerRadius = 14
-    }
+    private let mainProfile = UIView()
     
-    private let mainProfileImg = UIImageView().then{
-        $0.backgroundColor = .clear
-        $0.image = UIImage(named: "logo.png")!
-        $0.translatesAutoresizingMaskIntoConstraints = false
-        $0.clipsToBounds
-        $0.cornerRadius = 53.5
-        
-    }
-    private let mainProfileTag = MainLabel(type: .small3).then {
-        $0.text = "디바이드 공식 돼지"
-        $0.textColor = .mainOrange1
-    }
-    private let userNickName = MainLabel(type: .Point4).then {
-        $0.text = "룡룡"
-    }
+    private let mainProfileImg = UIImageView()
+    private let mainProfileTag = MainLabel(type: .small3)
+    private let userNickName = MainLabel(type: .Point4)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        setConstraint()
-        setComponentsConstraint()
+        setAttribute()
+        addView()
+        setLayout()
     }
-    
-    private func setConstraint(){
-        self.view.backgroundColor = .gray0
+    func setAttribute() {
+        view.backgroundColor = .viewBackgroundGray
+        mainProfile.do {
+            $0.borderColor = .mainYellow
+            $0.borderWidth = 3
+            $0.backgroundColor = .white
+            $0.cornerRadius = 14
+        }
+        mainProfileImg.do {
+            $0.backgroundColor = .clear
+            $0.image = UIImage(named: "logo.png")!
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.cornerRadius = 53.5
+        }
+        mainProfileTag.do {
+            $0.text = "디바이드 공식 돼지"
+            $0.textColor = .mainOrange1
+        }
+        userNickName.do {
+            $0.text = "룡룡"
+        }
     }
-    
-    private func setComponentsConstraint(){
-        let widthBase = UIScreen.main.bounds.width * 0.85
-        self.view.addSubview(mainProfile)
-        
+    func addView() {
+        view.addSubview(mainProfile)
+        view.addSubview(mainProfileImg)
         mainProfile.addSubview(mainProfileTag)
-        self.view.addSubview(mainProfileImg)
+    }
+    func setLayout() {
+        let widthBase = UIScreen.main.bounds.width * 0.85
         mainProfile.snp.makeConstraints { make in
             make.width.equalTo(widthBase)
             make.height.equalTo(167)
@@ -67,10 +69,10 @@ class ProfileViewController: UIViewController {
             make.centerX.equalToSuperview()
             make.top.equalTo(mainProfile).offset(-53.5)
         }
-        
         mainProfileImg.image =  mainProfileImg.image?.resize(toTargetSize: CGSize(width: 88, height: 88))
         mainProfileImg.layer.addShadow(location: .bottom, color: .gray0, opacity: 0.15, radius: 53.5)
+
     }
-    
+   
 
 }
