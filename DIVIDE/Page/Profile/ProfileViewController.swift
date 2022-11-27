c63e7814aa2c6cce8d06b89a7fb90fbbe81d7f09
Henry <wcbe9745@naver.com>
2022-11-28 02:16:27 +0900

    view configration 40% completed.

--- DIVIDE/Page/Profile/ProfileViewController.swift
+++ DIVIDE/Page/Profile/ProfileViewController.swift
@@ -11,10 +11,28 @@ import SnapKit
 class ProfileViewController: UIViewController {
     
     private let mainProfile = UIView()
-    
+    private let settingBtn = UIButton()
     private let mainProfileImg = UIImageView()
     private let mainProfileTag = MainLabel(type: .small3)
     private let userNickName = MainLabel(type: .Point4)
+    
+    private let followingCount = MainLabel(type: .Big2)
+    private let followingLabel = MainLabel(type: .Basics2)
+    private let mainProfileDivideBar = UIView()
+    private let followerCount = MainLabel(type: .Big2)
+    private let followerLabel = MainLabel(type: .Basics2)
+    
+    private let retrenchView = UIView()
+    private let retrenchCO2Label = MainLabel(type: .Basics1)
+    private let retrenchDeliveryFeeLabel = MainLabel(type: .Basics1)
+    
+    private let retrenchViewDivideHorizontal = UIView()
+    
+    private let retrenchCO2Value = MainLabel(type: .Big1)
+    private let retrenchCO2Gram = MainLabel(type: .Basics2)
+    private let retrenchViewDivideVertical = UIView()
+    private let retrenchDeliveryFeeValue = MainLabel(type: .Big1)
+    private let retrenchDeliveryFeeWon = MainLabel(type: .Basics2)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +48,15 @@ class ProfileViewController: UIViewController {
             $0.backgroundColor = .white
             $0.cornerRadius = 14
         }
+        settingBtn.do {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.setImage(UIImage(named: "설정.png"), for: .normal)
+            $0.imageView?.contentMode = .scaleAspectFit
+            $0.contentHorizontalAlignment = .center
+            $0.semanticContentAttribute = .forceRightToLeft
+            $0.contentVerticalAlignment = .center
+            $0.contentHorizontalAlignment = .leading
+        }
         mainProfileImg.do {
             $0.backgroundColor = .clear
             $0.image = UIImage(named: "logo.png")!
@@ -38,16 +65,79 @@ class ProfileViewController: UIViewController {
         }
         mainProfileTag.do {
             $0.text = "디바이드 공식 돼지"
+            $0.textAlignment = .center
             $0.textColor = .mainOrange1
         }
         userNickName.do {
+            $0.textAlignment = .center
             $0.text = "룡룡"
+        }
+        followingCount.do {
+            $0.textAlignment = .center
+            $0.text = "0"
+        }
+        followingLabel.do {
+            $0.textAlignment = .center
+            $0.text = "팔로잉"
+        }
+        mainProfileDivideBar.do {
+            $0.backgroundColor = .gray0
+        }
+        followerCount.do {
+            $0.textAlignment = .center
+            $0.text = "0"
+        }
+        followerLabel.do {
+            $0.textAlignment = .center
+            $0.text = "팔로워"
+        }
+        retrenchView.do {
+            $0.backgroundColor = .white
+            $0.cornerRadius = 26
+            $0.layer.addShadow(location: .all, color: .gray0, opacity: 0.85, radius: 26)
+        }
+        retrenchCO2Label.do {
+            $0.text = "절약한 탄소배출"
+            $0.textAlignment = .center
+        }
+        retrenchDeliveryFeeLabel.do {
+            $0.text = "절약한 배달비"
+            $0.textAlignment = .center
+        }
+        retrenchViewDivideHorizontal.do {
+            $0.backgroundColor = .gray0
+        }
+        retrenchCO2Value.do {
+            $0.text = "0"
+            $0.textColor = .mainOrange1
+            $0.textAlignment = .center
+        }
+        retrenchCO2Gram.do {
+            $0.text = "g"
+            $0.textColor = .mainOrange2
+        }
+        retrenchViewDivideVertical.do {
+            $0.backgroundColor = .gray0
+        }
+        retrenchDeliveryFeeValue.do {
+            $0.text = "5930"
+            $0.textColor = .mainOrange1
+            $0.textAlignment = .center
+        }
+        retrenchDeliveryFeeWon.do {
+            $0.text = "원"
+            $0.textColor = .mainOrange2
         }
     }
     func addView() {
         view.addSubview(mainProfile)
+        view.addSubview(settingBtn)
         view.addSubview(mainProfileImg)
-        mainProfile.addSubview(mainProfileTag)
+        view.addSubview(retrenchView)
+        
+        mainProfile.addSubviews([mainProfileTag, userNickName, followingCount, followingLabel, mainProfileDivideBar, followerCount, followerLabel])
+        
+        retrenchView.addSubviews([retrenchCO2Label, retrenchDeliveryFeeLabel, retrenchViewDivideHorizontal, retrenchCO2Value, retrenchCO2Gram, retrenchViewDivideVertical, retrenchDeliveryFeeValue, retrenchDeliveryFeeWon])
     }
     func setLayout() {
         let widthBase = UIScreen.main.bounds.width * 0.85
@@ -57,11 +147,10 @@ class ProfileViewController: UIViewController {
             make.centerX.equalToSuperview()
             make.top.equalToSuperview().offset(110)
         }
-        mainProfileTag.snp.makeConstraints { make in
-            make.width.equalTo(84)
-            make.height.equalTo(14)
-            make.centerX.equalToSuperview()
-            make.centerY.equalToSuperview().offset(-10)
+        settingBtn.snp.makeConstraints { make in
+            make.width.height.equalTo(23)
+            make.top.equalToSuperview().offset(71)
+            make.trailing.equalToSuperview().offset(-53)
         }
         mainProfileImg.snp.makeConstraints { make in
             make.width.equalTo(107)
@@ -70,8 +159,106 @@ class ProfileViewController: UIViewController {
             make.top.equalTo(mainProfile).offset(-53.5)
         }
         mainProfileImg.image =  mainProfileImg.image?.resize(toTargetSize: CGSize(width: 88, height: 88))
-        mainProfileImg.layer.addShadow(location: .bottom, color: .gray0, opacity: 0.15, radius: 53.5)
+        mainProfileImg.layer.addShadow(location: .all, color: .gray0, opacity: 0.15, radius: 53.5)
 
+        mainProfileTag.snp.makeConstraints { make in
+            make.width.equalTo(84)
+            make.height.equalTo(14)
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview().offset(-15)
+        }
+        userNickName.snp.makeConstraints { make in
+            make.width.equalTo(84)
+            make.height.equalTo(23)
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview()
+        }
+        followingCount.snp.makeConstraints { make in
+            make.width.equalTo(12)
+            make.height.equalTo(29)
+            make.leading.equalToSuperview().offset(76.5)
+            make.centerY.equalToSuperview().offset(20)
+        }
+        followingLabel.snp.makeConstraints { make in
+            make.width.equalTo(35)
+            make.height.equalTo(16)
+            make.centerY.equalToSuperview().offset(45)
+            make.leading.equalToSuperview().offset(65)
+        }
+        mainProfileDivideBar.snp.makeConstraints { make in
+            make.width.equalTo(3)
+            make.height.equalTo(34)
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview().offset(35)
+        }
+        followerCount.snp.makeConstraints { make in
+            make.width.equalTo(12)
+            make.height.equalTo(29)
+            make.trailing.equalToSuperview().offset(-76.5)
+            make.centerY.equalToSuperview().offset(20)
+        }
+        followerLabel.snp.makeConstraints { make in
+            make.width.equalTo(35)
+            make.height.equalTo(16)
+            make.centerY.equalToSuperview().offset(45)
+            make.trailing.equalToSuperview().offset(-65)
+        }
+        
+        retrenchView.snp.makeConstraints { make in
+            make.width.equalTo(mainProfile)
+            make.height.equalTo(132)
+            make.centerX.equalToSuperview()
+            make.centerY.equalTo(mainProfile).offset(166)
+        }
+        
+        retrenchCO2Label.snp.makeConstraints { make in
+            make.width.equalTo(85)
+            make.height.equalTo(17)
+            make.leading.equalToSuperview().offset(59)
+            make.top.equalToSuperview().offset(25)
+        }
+        retrenchDeliveryFeeLabel.snp.makeConstraints { make in
+            make.width.equalTo(85)
+            make.height.equalTo(17)
+            make.trailing.equalToSuperview().offset(-144)
+            make.top.equalToSuperview().offset(25)
+        }
+        retrenchViewDivideHorizontal.snp.makeConstraints { make in
+            make.width.equalToSuperview()
+            make.height.equalTo(2)
+            make.centerY.equalToSuperview().offset(-5)
+        }
+        
+        retrenchCO2Value.snp.makeConstraints { make in
+            make.width.equalTo(54)
+            make.height.equalTo(32)
+            make.top.equalTo(retrenchViewDivideHorizontal).offset(20)
+            make.centerX.equalToSuperview().offset(-87.5)
+        }
+        retrenchCO2Gram.snp.makeConstraints { make in
+            make.width.equalTo(12)
+            make.height.equalTo(18)
+            make.bottom.equalTo(retrenchCO2Value)
+            make.centerX.equalToSuperview().offset(-45)
+        }
+        retrenchViewDivideVertical.snp.makeConstraints { make in
+            make.width.equalTo(2)
+            make.height.equalTo(34)
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview().offset(20)
+        }
+        retrenchDeliveryFeeValue.snp.makeConstraints { make in
+            make.width.equalTo(54)
+            make.height.equalTo(32)
+            make.top.equalTo(retrenchCO2Value)
+            make.centerX.equalToSuperview().offset(87.5)
+        }
+        retrenchDeliveryFeeWon.snp.makeConstraints { make in
+            make.width.equalTo(12)
+            make.height.equalTo(16)
+            make.bottom.equalTo(retrenchCO2Value)
+            make.centerX.equalToSuperview().offset(129)
+        }
     }
    
 
